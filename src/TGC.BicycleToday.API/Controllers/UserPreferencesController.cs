a66d0a4f5f65b6f2b6af210c5fffe4bdf1c064f2
Asger Arnbjørn Thyregod <asger.thyregod@gmail.com>
2024-04-14 20:03:18 +0200

    Added barebone client and backend

---
+++ src/TGC.BicycleToday.API/Controllers/UserPreferencesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TGC.BicycleToday.API.Controllers;
+[Route("api/user/preferences")]
+[ApiController]
+public class UserPreferencesController : ControllerBase
+{
+	[HttpGet("{userId}")]
+	public Task Get(Guid userId)
+	{
+		throw new NotImplementedException();
+	}
+
+	[HttpPost("{userId}")]
+	public Task Create(Guid userId)
+	{
+		throw new NotImplementedException();
+	}
+
+	[HttpPut("{userId}")]
+	public Task Update(Guid userId)
+	{
+		throw new NotImplementedException();
+	}
+}
